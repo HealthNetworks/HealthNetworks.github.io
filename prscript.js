fe82c8a9fbca0bb5aad91af993d12e7dda387735
HummdG <hummd2001@gmail.com>
2024-04-23 19:34:12 +0100

    new

--- prscript.js
+++ prscript.js
@@ -1,35 +1,32 @@
-// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the modal
+    var modal = document.getElementById("bioModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // Function to display the modal with the clicked person's information
+    function displayBio(event) {
+        var person = event.target.closest(".person");
+        if (person) {
+            var bioText = person.dataset.name;
+            document.getElementById("bioContent").innerHTML = "<p>" + bioText + "</p>";
+            modal.style.display = "block";
+        }
+    }
 
-// Get all images with class "person"
-var images = document.querySelectorAll('.person img');
+    // Handle click event on the grid container
+    var gridContainer = document.getElementById("gridContainer");
+    gridContainer.addEventListener('click', displayBio);
 
-// Get the modal
-var modal = document.getElementById("bioModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Function to display the modal with the clicked image's alt text
-function displayBio(event) 
-{   console.log("Displaying bio...");
-    var bioText = event.target.alt;
-    document.getElementById("bioContent").innerHTML = "<p>" + bioText + "</p>";
-    modal.style.display = "block";
-}
-
-// Loop through each image and add a click event listener
-images.forEach(function(image) {
-    image.addEventListener('click', displayBio);
-});
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
