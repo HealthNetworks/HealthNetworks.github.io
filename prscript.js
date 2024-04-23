cfae4b6c8b9002a0cd38e8fd45788130fc3e09d1
HummdG <hummd2001@gmail.com>
2024-04-23 19:17:58 +0100

    new js

--- prscript.js
+++ prscript.js
@@ -1,39 +1,34 @@
-// prscript.js
+// script.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Get all bio buttons
-    var bioButtons = document.querySelectorAll('.bio-button');
+// Get all images with class "person"
+var images = document.querySelectorAll('.person img');
 
-    // Get the bio modal
-    var modal = document.getElementById("bioModal");
+// Get the modal
+var modal = document.getElementById("bioModal");
 
-    // Get the <span> element that closes the modal
-    var closeButton = document.querySelector(".modal .close");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-    // Add event listeners to all bio buttons
-    bioButtons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            // Get the position of the clicked button
-            var buttonRect = button.getBoundingClientRect();
+// Function to display the modal with the clicked image's alt text
+function displayBio(event) {
+    var bioText = event.target.alt;
+    document.getElementById("bioContent").innerHTML = "<p>" + bioText + "</p>";
+    modal.style.display = "block";
+}
 
-            // Set modal position next to the clicked button
-            modal.style.left = buttonRect.right + 'px';
-            modal.style.top = buttonRect.top + 'px';
-
-            // Display the modal
-            modal.style.display = "block";
-        });
-    });
-
-    // Close the modal when the close button is clicked
-    closeButton.addEventListener('click', function() {
-        modal.style.display = "none";
-    });
-
-    // Close the modal when clicking anywhere outside the modal content
-    window.addEventListener('click', function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
+// Loop through each image and add a click event listener
+images.forEach(function(image) {
+    image.addEventListener('click', displayBio);
 });
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
