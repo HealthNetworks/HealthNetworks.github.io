870c40bdfcd2a347493a601ae31e3639a9d9af09
HummdG <hummd2001@gmail.com>
2024-04-23 19:38:36 +0100

    new

--- prscript.js
+++ prscript.js
@@ -3,20 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     var modal = document.getElementById("bioModal");
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    
-    // Function to display the modal with the clicked person's information
-    function displayBio(event) {
+
+    // Handle click event on the grid container
+    var gridContainer = document.getElementById("gridContainer");
+    gridContainer.addEventListener('click', function(event) {
         var person = event.target.closest(".person");
         if (person) {
             var bioText = person.dataset.name;
             document.getElementById("bioContent").innerHTML = "<p>" + bioText + "</p>";
             modal.style.display = "block";
         }
-    }
-
-    // Handle click event on the grid container
-    var gridContainer = document.getElementById("gridContainer");
-    gridContainer.addEventListener('click', displayBio);
+    });
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
