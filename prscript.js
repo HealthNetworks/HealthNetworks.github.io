38ae29a36f57af05fde69b2eabf7c84784eea64e
HummdG <hummd2001@gmail.com>
2024-04-23 19:40:23 +0100

    new

--- prscript.js
+++ prscript.js
@@ -1,29 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the modal
-    var modal = document.getElementById("bioModal");
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+// script.js
 
-    // Handle click event on the grid container
-    var gridContainer = document.getElementById("gridContainer");
-    gridContainer.addEventListener('click', function(event) {
-        var person = event.target.closest(".person");
-        if (person) {
-            var bioText = person.dataset.name;
-            document.getElementById("bioContent").innerHTML = "<p>" + bioText + "</p>";
-            modal.style.display = "block";
-        }
-    });
+// Get all images with class "person"
+var images = document.querySelectorAll('.person img');
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+// Get the modal
+var modal = document.getElementById("bioModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Function to display the modal with the clicked image's alt text
+function displayBio(event) {
+    var bioText = event.target.alt;
+    document.getElementById("bioContent").innerHTML = "<p>" + bioText + "</p>";
+    modal.style.display = "block";
+}
+
+// Loop through each image and add a click event listener
+images.forEach(function(image) {
+    image.addEventListener('click', displayBio);
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-});
+}
